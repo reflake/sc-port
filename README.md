5bd64feab235bb55e68f6f84d32ad8cc99ae48d7
reflake <reflakeykt@gmail.com>
2024-08-31 23:29:24 +0500

    simple storage reading

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dependencies
+
+The library [CascLib](https://github.com/ladislav-zezula/CascLib) (credits to [ladislav-zezula](https://github.com/ladislav-zezula)) is required to build the game's engine
+
+# Installation
+
+Download the CascLib 2.1 source archive from [releases page](https://github.com/ladislav-zezula/CascLib/releases) and place its contents into CascLib folder of the project
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,66 @@
+#include <CascLib.h>
+#include <fileapi.h>
+#include <handleapi.h>
 #include <iostream>
+#include <minwindef.h>
+#include <ostream>
+#include <stdexcept>
+#include <winnt.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "Hello, world!";
+	if (argc < 3)
+	{
+		std::cout << "Two arguments required: <storage path> <output file>" << std::endl;
+		return -1;
+	}
+
+	std::string storagePath = argv[1];
+	std::string outputPath = argv[2];
+
+	HANDLE storage;
+
+	if (!CascOpenStorage(storagePath.c_str(), 0, &storage))
+	{
+		throw std::runtime_error("Couldn't open game storage");
+	}
+
+	HANDLE storageFile;
+
+	if (!CascOpenFile(storage, "sound/terran/marine/tmapss00.wav", 0, 0, &storageFile))
+	{
+		throw std::runtime_error("Couldn't read sound file");
+	}
+
+	auto outputFile = CreateFile(outputPath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
+
+	if (outputFile == INVALID_HANDLE_VALUE)
+	{
+		throw std::runtime_error("Couldn't create output file");
+	}
+
+	char  buffer[1000];
+	DWORD bytesRead;
+
+	do {
+
+		CascReadFile(storageFile, buffer, sizeof(buffer), &bytesRead);
+
+		DWORD bytesWritten;
+
+		if (bytesRead > 0)
+		{
+			WriteFile(outputFile, buffer, bytesRead, &bytesWritten, NULL);
+
+			if (bytesWritten != bytesRead)
+			{
+				throw std::runtime_error("Couldn't write to output file");
+			}
+		}
+
+	} while (bytesRead != 0);
+
+	CascCloseFile(storageFile);
+	CascCloseStorage(storage);
+	CloseHandle(outputFile);
 }
