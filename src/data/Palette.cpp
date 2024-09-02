a9035e4f362f7e6c011e79445f4eef3cac65ce93
reflake <reflakeykt@gmail.com>
2024-09-02 15:32:13 +0500

    image color palette data

---
+++ src/data/Palette.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+
+#include "Palette.hpp"
+
+namespace data {
+
+	Palette::Palette(WpeData& data)
+	{
+		for(int i = 0; i < PALETTE_SIZE; i++)
+		{
+			_colors[i] = data.colors[i];
+			_colors[i].a = 0xFF;
+		}
+
+		// First color of palette is transparent
+		_colors[0].a = 0x00;
+	}
+
+	const Color* Palette::GetColors() const
+	{
+		return _colors;
+	}
+}
---
+++ src/data/Palette.hpp
@@ -0,0 +1,26 @@
+#include <glm/vec4.hpp>
+
+namespace data
+{
+	typedef glm::vec<4, uint8_t> Color;
+
+	const int PALETTE_SIZE = 256;
+
+	struct WpeData
+	{
+		Color colors[PALETTE_SIZE];
+	};
+
+	class Palette
+	{
+	public:
+
+		Palette(WpeData& data);
+
+		const Color* GetColors() const;
+
+	private:
+
+		Color _colors[PALETTE_SIZE];
+	};
+}
