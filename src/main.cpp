276436ae8e70972a343cf95c99d1d6c716c26d71
reflake <reflakeykt@gmail.com>
2024-09-02 15:36:12 +0500

    read game tiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,28 @@
 #define SDL_MAIN_HANDLED
 
 #include <intrin.h>
-#include <SDL_events.h>
-#include <SDL_hints.h>
-#include <SDL_video.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL.h>
 
 #include <boost/format.hpp>
 #include <boost/format/format_fwd.hpp>
 #include <CascLib.h>
+#include <cstring>
 #include <fileapi.h>
 #include <handleapi.h>
 #include <iostream>
 #include <minwindef.h>
 #include <stdexcept>
 
-#include "meta/Unit.h"
+#include <SDL_events.h>
+#include <SDL_hints.h>
+#include <SDL_keycode.h>
+#include <SDL_pixels.h>
+#include <SDL_rect.h>
+#include <SDL_surface.h>
+#include <SDL_video.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL.h>
+
+#include "data/Palette.hpp"
 #include "filesystem/storage.hpp"
 #include "filesystem/storage_file.hpp"
 
@@ -80,14 +86,14 @@ void createWindow(App& app)
 		throwSdlError("Failed to create the renderer");
 	}
 
-	SDL_SetRenderDrawColor(app.renderer, 0x64, 0x95, 0xED, 0xFF);
-
 	app.screenSurface = SDL_GetWindowSurface(app.window);
 
 	if (!app.screenSurface)
 	{
 		throwSdlError("Couldn't get the screen of the window");
 	}
+	
+	SDL_FillRect( app.screenSurface, NULL, SDL_MapRGB( app.screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 }
 
 void freeWindow(App& app)
@@ -103,22 +109,120 @@ void freeWindow(App& app)
 	}
 }
 
+struct Tile
+{
+	uint8_t palPixels[64];
+};
+
+struct Page
+{
+	SDL_Surface* surface;
+};
+
+template<int L = 8>
+void createTilePages(App& app, Tile* tiles, int tileCount, std::vector<Page>& pages, data::Palette paletteData)
+{
+	auto pal = SDL_AllocPalette(256);
+	auto colors = reinterpret_cast<const SDL_Color*>(paletteData.GetColors());
+
+	SDL_SetPaletteColors(pal, colors, 0, 256);
+
+	for(int i = 0; i < tileCount; i+=L*L)
+	{
+		const int width = L * 8;
+		const int height = L * 8;
+		auto surface = SDL_CreateRGBSurface(0, width, height, 8, 0, 0, 0, 0);
+		SDL_SetSurfacePalette(surface, pal);
+
+		SDL_LockSurface(surface);
+
+		for(int j = 0; j < width; j+=8)
+		for(int k = 0; k < height; k++)
+		{
+			int tileIndex = i + j / 8 + (k / 8) * 8;
+
+			if (tileIndex >= tileCount)
+				continue;
+
+			auto& tile = tiles[tileIndex];
+
+			auto pixel = reinterpret_cast<uint8_t*>(surface->pixels) + j + k * width;
+
+			memcpy(pixel, &tile.palPixels[(k % 8) * 8], 8);
+		}
+
+		SDL_UnlockSurface(surface);
+
+		auto optimizedSurface = SDL_ConvertSurface(surface, app.screenSurface->format, 0);
+
+		SDL_FreeSurface(surface);
+
+		pages.push_back(Page(optimizedSurface));
+	}
+}
+
 int main(int argc, char* argv[])
 {
+	std::vector<Page> pages;
+
 	App app;
 
 	initSDL();
 	createWindow(app);
+	
+	{
+		filesystem::Storage storage(argv[1]);
+
+		// Read palette of tile set
+		data::WpeData wpeData;
+		storage.Read("TileSet/ashworld.wpe", wpeData);
+
+		data::Palette palette(wpeData);
+
+		filesystem::StorageFile tileSetFile;
+
+		// Read mini tile's data
+		storage.Open("TileSet/ashworld.vr4", tileSetFile);
+		
+		int tileDataSize = tileSetFile.GetFileSize();
+		int tileCount = tileDataSize / sizeof(Tile);
+		auto tiles = new Tile[tileCount];
+		tileSetFile.Read(tiles, tileDataSize);
+
+		createTilePages<13>(app, tiles, tileCount, pages, palette);
+
+		delete[] tiles;
+	}
 
 	SDL_Event event;
 
 	bool running = true;
+
+	int page = 0;
+				
+	SDL_Rect destRect { .x = 0, .y = 0, .w = 512, .h = 512 };
 
 	while(running)
 	while(SDL_PollEvent(&event))
 	{
 		switch(event.type)
 		{
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_UP)
+				{
+					page++;
+				}
+				else if (event.key.keysym.sym == SDLK_DOWN)
+				{
+					page--;
+				}
+
+				page = (page + pages.size()) % pages.size();
+
+				SDL_BlitScaled(pages[page].surface, NULL, app.screenSurface, &destRect);
+				SDL_UpdateWindowSurface(app.window);
+				break;
+
 			case SDL_QUIT:
 				running = false;
 				break;
