6c542b096e6a3b0f53dd560384466da1aa68071b
reflake <reflakeykt@gmail.com>
2024-09-06 15:20:56 +0500

    define images table and method to read it

---
+++ src/data/Images.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+
+#include "Images.hpp"
+#include "Sprite.hpp"
+
+namespace data
+{
+	void ReadImagesTable(filesystem::Storage& storage, ImagesTable& table)
+	{
+		filesystem::StorageFile file;
+
+		storage.Open("arr/images.dat", file);
+
+		assert(file.GetFileSize() == MAX_SPRITE_AMOUNT * 38);
+
+		file.Read(table.grpID);
+		file.Read(table.turns);
+		file.Read(table.selectable);
+		file.Read(table.useIScript);
+		file.Read(table.drawIfCloaked);
+		file.Read(table.drawFunction);
+		file.Read(table.remapping);
+		file.Read(table.iScriptID);
+		file.Read(table.shieldOverlay);
+		file.Read(table.attackOverlay);
+		file.Read(table.damageOverlay);
+		file.Read(table.specialOverlay);
+		file.Read(table.landingOverlay);
+		file.Read(table.liftOffOverlay);
+	}
+}
---
+++ src/data/Images.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdint>
+
+#include "../filesystem/Storage.hpp"
+
+namespace data
+{
+	const int MAX_IMAGES_AMOUNT = 999;
+
+	enum class PaletteRemap : uint8_t { 
+			None = 0, 
+			OrangeFire, 
+			GreenFire, 
+			BlueFire,
+			BlueExplosion,
+			Special,
+	 };
+
+	struct ImagesTable
+	{
+		uint32_t grpID[MAX_IMAGES_AMOUNT];
+		uint8_t  turns[MAX_IMAGES_AMOUNT];
+		bool     selectable[MAX_IMAGES_AMOUNT];
+		bool     useIScript[MAX_IMAGES_AMOUNT];
+		bool     drawIfCloaked[MAX_IMAGES_AMOUNT];
+		uint8_t  drawFunction[MAX_IMAGES_AMOUNT];
+		uint32_t iScriptID[MAX_IMAGES_AMOUNT];
+		uint32_t shieldOverlay[MAX_IMAGES_AMOUNT];
+		uint32_t attackOverlay[MAX_IMAGES_AMOUNT];
+		uint32_t damageOverlay[MAX_IMAGES_AMOUNT];
+		uint32_t specialOverlay[MAX_IMAGES_AMOUNT];
+		uint32_t landingOverlay[MAX_IMAGES_AMOUNT];
+		uint32_t liftOffOverlay[MAX_IMAGES_AMOUNT];
+
+		PaletteRemap remapping[MAX_IMAGES_AMOUNT];
+	};
+
+	static_assert(sizeof(ImagesTable) == 37964, "ImagesTable size should be equal to 37964");
+
+	extern void ReadImagesTable(filesystem::Storage& storage, ImagesTable& table);
+};
