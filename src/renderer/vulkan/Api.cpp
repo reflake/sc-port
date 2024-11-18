46da8528365f727c4ad1bf5881b8cb1c2397b584
reflake <reflakeykt@gmail.com>
2024-11-18 16:57:48 +0500

    window type check

--- src/renderer/vulkan/Api.cpp
+++ src/renderer/vulkan/Api.cpp
@@ -4,12 +4,18 @@
 #include <memory>
 
 #include <SDL_video.h>
+#include <stdexcept>
 
 namespace renderer::vulkan
 {
 	std::unique_ptr<A_Graphics> CreateGraphics(void* window)
 	{
 		SDL_Window* sdlWindow = reinterpret_cast<SDL_Window*>(window);
+
+		if (sdlWindow == nullptr)
+		{
+			throw std::runtime_error("Instance of window is not supported");
+		}
 
 		return std::make_unique<Graphics>(sdlWindow);
 	}
